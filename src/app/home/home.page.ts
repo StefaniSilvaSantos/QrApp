2eedc6c295445bc757706d6c3a635bd5e39d4005
Stefani da Silva Santos <santosstefani253@gmail.com>
2021-11-12 16:41:39 -0300

    App com qrcode funcionando

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 
+import { QRScanner, QRScannerStatus } from '@ionic-native/qr-scanner/ngx';
+import { Alert } from 'selenium-webdriver';
+
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
@@ -7,6 +10,43 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  scanner: any;
+  content: HTMLElement;
 
-}
+  resultado = '';
+
+  constructor(private qrScanner: QRScanner) {}
+
+  lerQRCode() {
+  // Optionally request the permission early
+this.qrScanner.prepare()
+.then((status: QRScannerStatus) => {
+   if (status.authorized) {
+     // camera permission was granted
+    this.content = document.getElementsByTagName('ion-content')[0];
+
+    this.content.style.opacity = '0';
+
+     // start scanning
+     this.scanner = this.qrScanner.scan().subscribe((text: string) => {
+       console.log('Scanned something', text);
+
+       this.content.style.opacity = '1';
+
+       this.qrScanner.hide(); // hide camera preview
+       this.scanner.unsubscribe(); // stop scanning
+     });
+
+   } else if (status.denied) {
+     // camera permission was permanently denied
+     // you must use QRScanner.openSettings() method to guide the user to the settings page
+     // then they can grant the permission from there
+     alert('NÃO É PARA NEGAR, BURRO !!!');
+   } else {
+     // permission was denied, but not permanently. You can ask for permission again at a later time.
+     alert('NÃO É PARA NEGAR. tente outra vez !!!');
+   }
+  })
+  .catch((e: any) => console.log('Error is', e));
+  }
+ }
